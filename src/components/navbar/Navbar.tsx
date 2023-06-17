da111236bd9ea7e981e06546fbdf84a668afa5db
mfcastilho <fredericocastilho@hotmail.com>
2023-06-17 04:36:34 -0300

    feat: navbar finalized

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,33 +8,15 @@ function Navbar(){
 
      return(
           <div className="navbar">
-               <motion.div 
-                         initial="hidden"
-                         whileInView="visible"
-                         viewport={{ once: true, amount: 0.5 }}
-                         transition={{ delay: 0.2, duration: 0.5 }}
-                         variants={{
-                         hidden: { opacity: 0, x: -50 },
-                         visible: { opacity: 1, x: 0 },
-                    }}
-               >         
+                    
                     <div className="right-part">
                          <h1>Portifólio</h1>
                     </div>
                     
                     
-               </motion.div>
+             
 
-               <motion.div 
-                         initial="hidden"
-                         whileInView="visible"
-                         viewport={{ once: true, amount: 0.5 }}
-                         transition={{ delay: 0.2, duration: 0.5 }}
-                         variants={{
-                         hidden: { opacity: 0, x: 50 },
-                         visible: { opacity: 1, x: 0 },
-                    }}
-               >
+              
 
                     <div className="left-part">
                               <ul>
@@ -44,7 +26,7 @@ function Navbar(){
                               </ul>
 
                     </div>
-               </motion.div>
+             
           </div>
      );
 }
--- src/components/toppart/TopPart.tsx
+++ src/components/toppart/TopPart.tsx
@@ -1,19 +1,31 @@
-import "./topPart.css"
+import "./topPart.css";
+import {motion} from "framer-motion";
 
 function TopPart(){
 
      return(
           <div className="top-part">
-               <div className="left-part">
-                    <h2>Olá, meu nome é Mario Frederico</h2>
-                    <h6>Sou Desenvovledor Web</h6>
-                    <ul>
-                         <li><a href="">Github</a></li>
-                         <li><a href="">Linkedin</a></li>
-                         <li><a href="">Instagram</a></li>
-                         <li><a href="">Currículo</a></li>
-                    </ul>
-               </div>
+               <motion.div 
+                         initial="hidden"
+                         whileInView="visible"
+                         viewport={{ once: true, amount: 0.5 }}
+                         transition={{ delay: 0.2, duration: 0.5 }}
+                         variants={{
+                         hidden: { opacity: 0, x: -50 },
+                         visible: { opacity: 1, x: 0 },
+                    }}
+               > 
+                    <div className="left-part">
+                         <h2>Olá, meu nome é Mario Frederico</h2>
+                         <h6>Sou Desenvovledor Web</h6>
+                         <ul>
+                              <li><a href="">Github</a></li>
+                              <li><a href="">Linkedin</a></li>
+                              <li><a href="">Instagram</a></li>
+                              <li><a href="">Currículo</a></li>
+                         </ul>
+                    </div>
+               </motion.div>
                <div className="right-part">
                     <img src="../../../public/mario-foto-linkedin.png" alt="" />
                </div>
