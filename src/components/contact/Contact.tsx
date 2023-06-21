18975baf95e02335805915c99dd55b4155feac8d
mfcastilho <fredericocastilho@hotmail.com>
2023-06-21 04:10:33 -0300

    feat: integrating the form with mailchimp

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -8,12 +8,14 @@ function Contact(){
                <div className="contact-elements-box">
                     <p>Quer entrar em contato comigo?</p>
                     <p className="paragraph2">Preencha o formulário entrarei em contato o mais rápido possível.</p>
-                    <form action="">
-                         <input type="text" name="name" placeholder="Nome:"/>
-                         <input type="email" name="email" placeholder="Email:"/>
-                         <input type="text" name="subject" placeholder="Assunto:"/>
-                         <textarea name="message" id="" placeholder="Sua mensagem:"></textarea>
+                    <form action="https://hotmail.us8.list-manage.com/subscribe/post?u=2ce863bd9b2eb5d7ac800d486&amp;id=ddfa824621&amp;f_id=006876e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
+                         <input type="text" name="NAME" placeholder="Nome:" required/>
+                         <input type="email" name="EMAIL" placeholder="Email:" required/>
+                         <input type="text" name="SUBJECT" placeholder="Assunto:" required/>
+                         <textarea name="MESSAGE" id="" placeholder="Sua mensagem:" required></textarea>
+                         <button type="submit">Enviar</button>
                     </form>
+                    
                </div>
           </div>
      );
