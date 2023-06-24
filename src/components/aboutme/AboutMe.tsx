78f58a050ccd27019df2cb4fad38199696f174fc
mfcastilho <fredericocastilho@hotmail.com>
2023-06-23 23:24:10 -0300

    feat: desktop model project finalized

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -19,14 +19,11 @@ function AboutMe(){
                     <div className="about-me-description">
                          
                          <p>"Sou graduado em análise e desenvolvimento de sistemas pela Unicesumar e formado no curso de desenvolvimento web fullstack pela Digital House. "</p>
-                         <br />
-                         <br />
+
                          <p>" Atualmente sou aluno do programa One Oracle Next Education especialização Frontend, fornecido pela Alura e do programa 1000 bolsas iFood especialização Backend, fornecido pela Cubos Academy. "</p>
-                         <br />
-                         <br />
+
                          <p>" Tenho conhecimento técnico em Typescript, JavaScript, Node.js, Express, Sequelize, bancos de dados relacionais, React, HTML, CSS, além de familiaridade com Java e Spring Boot. "</p>
-                         <br />
-                         <br />
+
                          <p>" Sou proativo, comunicativo, disciplinado e organizado. Tenho habilidade em pesquisar soluções e estou sempre aberto para aprender, evoluir e ajudar. "</p>
                     </div>
                </div>
