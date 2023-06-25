26ca39433626d18f5fc3e586f01e1bce7d24763a
mfcastilho <fredericocastilho@hotmail.com>
2023-06-25 04:24:00 -0300

    feat: transforming the AcademicBackground component in responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import TopPart from './components/toppart/TopPart';
 import AboutMe from './components/aboutme/AboutMe';
 import Skills from './components/skills/Skills';
-// import AcademicBackground from './components/academic-background/AcademicBackground';
+import AcademicBackground from './components/academic-background/AcademicBackground';
 // import MyProjects from './components/my-projects/MyProjects';
 // import Contact from './components/contact/Contact';
 
@@ -14,8 +14,8 @@ function App() {
                <TopPart />
                <AboutMe />
                <Skills />
-               {/* <AcademicBackground />
-               <MyProjects />
+               <AcademicBackground />
+               {/* <MyProjects />
                <Contact/> */}
           </div>
      )
