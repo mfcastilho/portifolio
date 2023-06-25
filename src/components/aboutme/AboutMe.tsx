20f0a1ef98b6a631474b96db7b2bfcb6c07c8f64
mfcastilho <fredericocastilho@hotmail.com>
2023-06-25 02:18:34 -0300

    feat: transforming the AboutMe component in responsive

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -14,19 +14,19 @@ function AboutMe(){
                }}
           
           >
-               <div>
-                    <h2>Sobre mim</h2>
-                    <div className="about-me-description">
-                         
-                         <p>"Sou graduado em análise e desenvolvimento de sistemas pela Unicesumar e formado no curso de desenvolvimento web fullstack pela Digital House. "</p>
 
-                         <p>" Atualmente sou aluno do programa One Oracle Next Education especialização Frontend, fornecido pela Alura e do programa 1000 bolsas iFood especialização Backend, fornecido pela Cubos Academy. "</p>
+               <h2>Sobre mim</h2>
+               <div className="about-me-description">
+                    
+                    <p>"Sou graduado em análise e desenvolvimento de sistemas pela Unicesumar e formado no curso de desenvolvimento web fullstack pela Digital House. "</p>
 
-                         <p>" Tenho conhecimento técnico em Typescript, JavaScript, Node.js, Express, Sequelize, bancos de dados relacionais, React, HTML, CSS, além de familiaridade com Java e Spring Boot. "</p>
+                    <p>" Atualmente sou aluno do programa One Oracle Next Education especialização Frontend, fornecido pela Alura e do programa 1000 bolsas iFood especialização Backend, fornecido pela Cubos Academy. "</p>
 
-                         <p>" Sou proativo, comunicativo, disciplinado e organizado. Tenho habilidade em pesquisar soluções e estou sempre aberto para aprender, evoluir e ajudar. "</p>
-                    </div>
+                    <p>" Tenho conhecimento técnico em Typescript, JavaScript, Node.js, Express, Sequelize, bancos de dados relacionais, React, HTML, CSS, além de familiaridade com Java e Spring Boot. "</p>
+
+                    <p>" Sou proativo, comunicativo, disciplinado e organizado. Tenho habilidade em pesquisar soluções e estou sempre aberto para aprender, evoluir e ajudar. "</p>
                </div>
+
           </motion.div>
      );
 }
