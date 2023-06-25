d29ba1004b81cbc600cf5a34f652bafd88baf991
mfcastilho <fredericocastilho@hotmail.com>
2023-06-25 03:14:15 -0300

    feat: limiting the min-width to 25.6rem in the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 
 import TopPart from './components/toppart/TopPart';
 import AboutMe from './components/aboutme/AboutMe';
-// import Skills from './components/skills/Skills';
+import Skills from './components/skills/Skills';
 // import AcademicBackground from './components/academic-background/AcademicBackground';
 // import MyProjects from './components/my-projects/MyProjects';
 // import Contact from './components/contact/Contact';
@@ -13,8 +13,8 @@ function App() {
           <div className='app'>
                <TopPart />
                <AboutMe />
-               {/* <Skills />
-               <AcademicBackground />
+               <Skills />
+               {/* <AcademicBackground />
                <MyProjects />
                <Contact/> */}
           </div>
